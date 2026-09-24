7de904d2f12697a2e613b4617318c199741fbbf8
mark <1415994589@qq.com>
2026-09-24 17:55:59 +0800

    Update portfolio site

--- _worker.js
+++ _worker.js
@@ -1,9 +1,15 @@
 export default {
-  fetch(request, env) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
     if (url.hostname === "www.huangrenjie.com") {
       url.hostname = "huangrenjie.com";
+      return Response.redirect(url.toString(), 301);
+    }
+
+    const m = url.pathname.match(/^\/articles\/([^/]+)\.md$/i);
+    if (m) {
+      url.pathname = `/articles/${m[1]}/`;
       return Response.redirect(url.toString(), 301);
     }
 
